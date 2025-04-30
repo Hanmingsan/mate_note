a30ab62fde4c615d4aea822d747487bd026f9257
humming <hanmingsan@outlook.com>
2025-04-30 07:19:05 -0400

    reshape backend api

--- backend/api/main.py
+++ backend/api/main.py
@@ -9,5 +9,9 @@ async def root():
     return{"message": "hello world!"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
-    return{"itemid": item_id + 'love u'}
+async def read_item(item_id: int):
+    return{"itemid": item_id + 114514}
+
+@app.get("/search_student_by_name/{name}")
+async def search_student_by_name(name: str):
+    return{name}
